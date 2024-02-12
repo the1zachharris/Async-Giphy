a28ea1117c504626ac50f345119e46a82983283e
Zachary Harris <the1zachharris@gmail.com>
2024-02-12 11:52:37 -0600

    gitImage function logs the url of the first image

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 require('dotenv').config();
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+//const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=dogs&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
+
+const getImage = async query => {
+    try{
+        const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`);
+        const data = await response.json();
+        const url = await data.data[0].url;
+        console.log(url);
+    } catch (err) {
+        console.error(err)
+    }
+    
+
+};
+
+getImage('dogs');
 
 // Print out value of API key stored in .env file
-console.log(process.env.API_KEY)
+//console.log(process.env.API_KEY);
